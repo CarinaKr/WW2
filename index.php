cfe1af4fd7c75c2ab75b298af74290e3cb4c8774
rexyhn <rexyhn3@googlemail.com>
2017-05-09 14:59:24 +0200

    Bootstrap Slider/Functions

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" type="text/css" href="leaflet/leaflet.css">
+		<!-- Bootstrap -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.min.css">
 		
 	</head>
 
@@ -23,7 +25,12 @@
 		<div id="map">
 			<!-- <img id="mapWindow" alt="map" src="Images/platzhalter.png"> -->
 		</div>
-		
+		<div id="slide">
+			<!-- <input id="ex13" class="slider" type="text" data-slider-min="1939" data-slider-max="1945" data-slider-ticks="[0, 1, 2, 3, 4, 5, 6]" data-slider-ticks-snap-bounds="1" data-slider-ticks-labels='["1939", "1940", "1941", "1942", "1943", "1944", "1945"]' data-slider-value="0" /> -->
+		</div>
+		<!-- Bootstrap -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/bootstrap-slider.js"></script>
+		<script type="text/javascript" language="JavaScript" src="slider.js"></script>
 		<script type="text/javascript" src="leaflet/leaflet.js"></script>
 		<script type="text/javascript" src="map.js"></script>
 	</body>
---
+++ slider.js
@@ -0,0 +1,41 @@
+//Slider Funktionen
+	var slider = new Slider("#ex13");
+	//Muss noch ausgefüllt werden
+	slider.on("slide", function(sliderValue) {
+	//1945
+	if(sliderValue==6){
+		
+		
+	}
+	//1944
+	else if(sliderValue==5){
+		
+		
+	}
+	//1943
+	else if(sliderValue==4){
+		
+		
+	}
+	//1942
+	else if(sliderValue==3){
+		
+		
+	}
+	//1941
+	else if(sliderValue==2){
+		
+		
+	}
+	//1940
+	else if(sliderValue==1){
+		
+		
+	}
+	//1939
+	else if(sliderValue==0){
+		
+		
+	}
+});
+
