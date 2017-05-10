84e20147b445c930898505fc45561e93971d855d
SaschaLK <SaschaL.Kufahl@gmail.com>
2017-05-10 14:05:40 +0200

    Stable V1 - Index + CSS

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 	<head>
 		<title>WW2 Navigator</title>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="styles.css">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" type="text/css" href="leaflet/leaflet.css">
-		<!-- Bootstrap -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/css/bootstrap-slider.min.css">
 		
 	</head>
 
@@ -22,13 +21,15 @@
 		</header>
 		
 		<!-- add proper Image -->
-		<div id="map">
-			<!-- <img id="mapWindow" alt="map" src="Images/platzhalter.png"> -->
-		</div>
+		<div id="map"></div>
 		<div id="slide">
-			<!-- <input id="ex13" class="slider" type="text" data-slider-handle="custom" data-slider-min="1939" data-slider-max="1945" data-slider-ticks="[0, 1, 2, 3, 4, 5, 6]" data-slider-ticks-snap-bounds="1" data-slider-ticks-labels='["1939", "1940", "1941", "1942", "1943", "1944", "1945"]' data-slider-value="0" /> -->
+			<input id="ex13" class="slider" type="text" data-slider-handle="custom" 
+				data-slider-min="1939" data-slider-max="1945" 
+				data-slider-ticks="[0, 1, 2, 3, 4, 5, 6]" 
+				data-slider-ticks-snap-bounds="1" 
+				data-slider-ticks-labels='["1939", "1940", "1941", "1942", "1943", "1944", "1945"]' 
+				data-slider-value="0" />
 		</div>
-		<!-- Bootstrap -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.8.0/bootstrap-slider.js"></script>
 		<script type="text/javascript" language="JavaScript" src="slider.js"></script>
 		<script type="text/javascript" src="leaflet/leaflet.js"></script>
