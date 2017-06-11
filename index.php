c85a8ba302a7dda0523bfd629441316e2cd74e9c
CarinaKr <carina.krafft@t-online.de>
2017-06-11 21:36:06 +0200

    smooth borders around map

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 	<body>		
 		<?php include 'PHP/header.php';	?>
 		
-		<img id="mapBorder" alt="mapBorder" src="Images/OldPaperMap.jpg">
+	    <img id="mapBorder" alt="mapBorder" src="Images/OldPaperMap.jpg">
 		<img id="mapBorderTop" alt="mapBorderTop" src="Images/OldPaperMapTop.png">
-		<img id="mapBorderRight" alt="mapBorderTop" src="Images/OldPaperMapTop.png">
+		<img id="mapBorderRight" alt="mapBorderRight" src="Images/OldPaperMapRight.png">
+		<img id="mapBorderBottom" alt="mapBorderBottom" src="Images/OldPaperMapBottom.png">
+		<img id="mapBorderLeft" alt="mapBorderLeft" src="Images/OldPaperMapLeft.png">
 		<div id="map"></div>
 		<div id="slide">
 			<input id="ex13" class="slider" type="text" data-slider-handle="custom" 
