93eb37f664a4f9551de2c58a25a7c155080e40c7
CarinaKr <carina.krafft@t-online.de>
2017-06-19 17:57:52 +0200

    bla3

--- PHP/database.php
+++ PHP/database.php
@@ -1,6 +1,8 @@
 <?php
 
-	$conn=new mysqli("pstud0","aca083","aca083","aca083");
+
+
+	$conn=new mysqli("pstud0.mt.haw-hamburg.de","aca083","aca083","aca083");
 	//mysqli_select_db("rdb-test");
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
